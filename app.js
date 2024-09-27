538ed8c07f313e81b32024c0ab7f343f9c1b4e81
vinayarva <vinaykumararva13@gmail.com>
2024-09-27 20:05:01 +0530

    final verion of the simple chatting

--- app.js
+++ app.js
@@ -1,48 +1,53 @@
-const express =  require("express")
-const bodyparser =  require("body-parser")
-const fs =  require("fs")
-const cookieParser =  require("cookie-parser")
+const express = require("express");
+const bodyparser = require("body-parser");
+const fs = require("fs");
 
+app = express();
 
-app = express()
+app.use(bodyparser.urlencoded({ extended: false }));
 
+app.get("/favicon.ico", (req, res) => res.status(204).end());
 
-app.use(bodyparser.urlencoded({extended:false}))
-app.use(cookieParser());
+app.get("/login", (req, res, next) => {
+  res.send(
+    `<form onsubmit="localStorage.setItem('username', document.getElementById('username').value)" action='/log' method='POST'>
+        <input type='text' id='username' placeholder='username' name='username'>
+        <button type='submit'>Login</button>
+    </form>`
+  );
+});
 
+app.post("/log", (req, res, next) => {
+  res.redirect("/room");
+});
 
-let username = "Unknown"
+app.get("/room", (req, res, next) => {
+  fs.readFile("./message.txt", "utf-8", (err, data) => {
+    if (err) return console.log(err);
 
-app.get('/favicon.ico', (req, res) => res.status(204).end());
+    return res.send(
+      `<p>${data}</p>
+      <form onsubmit="document.getElementById('hiddenUsername').value = localStorage.getItem('username')" action='/room' method='POST'>
+        <input type='hidden' id='hiddenUsername' name='username'>
+        <input type='text' id='message' placeholder='message' name='message'>
+        <button type='submit'>Send</button>
+      </form>`
+    );
+  });
+});
 
-app.get("/login",(req,res,next)=>{
-    res.send("<form action='/log' method='POST'><input type='text' placeholder='username' name='username'><button type='submit'>Login</button></form>")
-})
+app.post("/room", (req, res, next) => {
+  const message = req.body.message;
+  const usernameFromLocalStorage = req.body.username;
 
-app.post("/log",(req,res,next)=>{
-    username =  {...req.body}
-    res.cookie('username', username.username, { maxAge: 900000, httpOnly: true });
-    res.redirect("/room")
-})
+  const text = usernameFromLocalStorage + ": " + message + " ";
+  fs.appendFile("message.txt", text, (err) => {
+    if (err) return console.log(err);
 
-app.get("/room",(req,res,next)=>{   
+    return res.redirect("/room");
+  });
+});
 
-    fs.readFile("./message.txt","utf-8",(err,data)=>{
-            if(err) return console.log(err)
-
-            return res.send(`<p>${data}</p><form action='/room' method='POST'><input type='text' placeholder='message' name='message'><button type='submit'>Send</button></form>`)
-         })   
-
-})
-
-app.post("/room",(req,res,next)=>{
-    const message = {...req.body}
-    const usernameFromCookie = req.cookies.username || 'Unknown';
-    const text =  usernameFromCookie + ": " + message.message +" "
-     fs.appendFile("message.txt",text,(err)=>{
-        if(err) return console.log(err)
-
-            return res.redirect("/room")
-     })
-})
-app.listen(3000)
+app.listen(3000, () => {
+  console.log("Server running at http://localhost:3000");
+});
